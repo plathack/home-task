ff4eab82051bb94dc30dd283ff4660e491e1516d
Ivan Efimcev <plathack228@yandex.ru>
2022-09-09 18:45:59 +0500

    fix 1 task

--- 1.sql
+++ 1.sql
@@ -1,31 +1,38 @@
-select distinct c.tin,
-sum
+select c.tin,
+sum 
 (
-	case when t.type = 'loan' then t.amount else 0 end 
-	- case when t.type = 'loan_repayment' then t.amount else 0 end
-	+ case when t.type = 'interest' then t.amount else 0 end
-	- case when t.type = 'interes_repayment' then t.amount else 0 end
-) over(partition by t.customer_id) as portfolio,
-sum
+	case 
+	when t.type = 'loan' then t.amount 
+	when t.type = 'loan_repayment' then -t.amount
+	when t.type = 'interest' then t.amount
+	when t.type = 'interes_repayment' then -t.amount 
+	else 0 end
+) as portfolio,
+(sum 
 (
-	case when t.type = 'loan' then t.amount else 0 end 
-	- case when t.type = 'loan_repayment' then t.amount else 0 end
-	+ case when t.type = 'interest' then t.amount else 0 end
-	- case when t.type = 'interes_repayment' then t.amount else 0 end
-) over(partition by t.customer_id) 
+	case 
+	when t.type = 'loan' then t.amount 
+	when t.type = 'loan_repayment' then -t.amount
+	when t.type = 'interest' then t.amount
+	when t.type = 'interes_repayment' then -t.amount 
+	else 0 end
+) 
 / 
-(select 
-  sum(
-	case when t.type = 'loan' then t.amount else 0 end 
-	- case when t.type = 'loan_repayment' then t.amount else 0 end
-	+ case when t.type = 'interest' then t.amount else 0 end
-	- case when t.type = 'interes_repayment' then t.amount else 0 end
-) from tbl_loan_transaction t) * 100 as percent
+sum(sum 
+(
+	case 
+	when t.type = 'loan' then t.amount 
+	when t.type = 'loan_repayment' then -t.amount
+	when t.type = 'interest' then t.amount
+	when t.type = 'interes_repayment' then -t.amount 
+	else 0 end
+)) over() * 100) as percent
 from tbl_customer c
 inner join tbl_loan_transaction t 
 on t.customer_id = c.id
-group by c.tin, t.type, t.amount, t.customer_id
+group by  c.tin
 order by portfolio desc
+
 
 
 
